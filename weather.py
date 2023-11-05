8996b6a85a58bb6c07e32db60b707e7414c7e8ba
caoimhejyoti <caoimhejyoti@gmail.com>
2023-11-05 18:17:41 +0800

    tiding of code

--- weather.py
+++ weather.py
@@ -24,16 +24,11 @@ def convert_date(iso_string):
     Returns:
         A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
     """
-    # print(f"iso_string: {iso_string}") #expected value
     provided_date = iso_string.split("T")[0]
-    # print(f"provided_date: {provided_date}") #expected value
     date_format = "%Y-%m-%d"
     raw_date = datetime.strptime(provided_date, date_format)
-    # print(f" raw_date: {raw_date}") #expected value
     date_conversion = (raw_date.strftime("%A %d %B %Y"))
-    # print(f"date_conversion: {date_conversion}") #expected value
     return(date_conversion)
-# print(convert_date("2021-10-01T07:00:00+08:00")) #expected value
 
 # COMPLETE!
 def convert_f_to_c(temp_in_farenheit):
@@ -51,7 +46,6 @@ def convert_f_to_c(temp_in_farenheit):
 # COMPLETE!
 def calculate_mean(weather_data):
     """Calculates the mean value from a list of numbers.
-
     Args:
         weather_data: a list of numbers.
     Returns:
@@ -60,16 +54,10 @@ def calculate_mean(weather_data):
     convert_data = []
     for element in weather_data:
         convert_data.append(float(element))
-    # print(f"convert_data: {convert_data}") #expected value
-    # print(f"convert_data type: {type(convert_data [0])}") #expected value
     length_of_input= len(convert_data) 
-    # print(f"length_of_input: {length_of_input}") #expected value
     sum_of_input = sum(convert_data)
-    # print(f"fum_of_input: {sum_of_input}") #expected value
     mean = sum_of_input/length_of_input
-    # print(f"mean: {mean}") #expected value
     return mean
-# print(calculate_mean([49, 57, 56, 55, 53]))
 
 # COMPLETE! 
 def load_data_from_csv(csv_file):
@@ -230,4 +218,3 @@ def generate_daily_summary(weather_data):
         )
     print(summary)
     return summary
-    
